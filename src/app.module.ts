b07cd3b052221111728794ff9a4c86d4a53ccd6a
Amir <mikelsonj000@gmail.com>
2023-08-29 13:09:44 +0400

    start project

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ChatModule } from './chat/chat.module';
+import { MessageModule } from './message/message.module';
+
+@Module({
+  imports: [ChatModule, MessageModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
